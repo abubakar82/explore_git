96f760c587dfb06e756527042a6658765c0e3837
byavv <aksslavvv@gmail.com>
2015-11-14 00:40:23 +0300

    init

---
+++ server.js
@@ -0,0 +1,24 @@
+var express = require("express"),
+	app = express(),
+	path = require("path");
+	
+app.get("/", function(req, res){
+	res.sendFile(path.join(__dirname, 'index.html'));
+})
+
+app.get("/ch1", function(req, res){
+	res.sendFile(path.join(__dirname, 'ch1.html'));
+})
+
+app.get("/ch3", function(req, res){
+	res.sendFile(path.join(__dirname, 'ch3.html'));
+})
+
+app.get("/course", function(req, res){
+	res.sendFile(path.join(__dirname, 'course.html'));
+})
+
+app.use("/assets", express.static(__dirname + '/assets'));
+
+app.listen(3333);
+console.log("server got started");
